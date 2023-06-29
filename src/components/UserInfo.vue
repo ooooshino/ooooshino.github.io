de4b910f4fadabddec42708ab5122e9f9b5fc43b
ooooshino <tangyusen9@gmail.com>
2023-06-29 16:16:52 +0800

    初始化

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="info">
+    <div>
+      <h3>ooooshino</h3>
+      <ul>
+        <li>1999 / 男 / 广东xx</li>
+        <li>本科 / 广东东软学院计算机系 / 23届应届生</li>
+        <li>技术博客：https://oshino.cc</li>
+        <li>期望职位：web前端开发工程师</li>
+        <li>手机：131-xxxx-xxxx（微信同号）</li>
+        <li>邮箱：xxxx@gmail.com</li>
+      </ul>
+    </div>
+    <div>
+      <img src="../assets/avatar.png" alt="avatar" />
+    </div>
+  </section>
+</template>
+
+<style lang="postcss" scoped>
+</style>
