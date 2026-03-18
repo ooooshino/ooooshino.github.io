1a39e651aeba8ebd5eaf803eea087e421586e6c5
ooooshino <tangyusen9@gmail.com>
2026-03-19 00:30:10 +0800

    fix(简历): 修复信息区移动端布局

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,14 +1,14 @@
 <template>
   <section>
     <div>
-      <h3 class="text-center sm:text-left">Kent</h3>
+      <h3 class="text-center sm:text-left break-words">Kent</h3>
       <ul class="flex flex-wrap">
-        <li class="w-1/3">xx（计算机相关专业）</li>
-        <li class="w-1/3">期望职位：前端开发工程师</li>
-        <li class="w-1/3">工作年限：3年+</li>
-        <li class="w-1/3">技术博客：xx</li>
-        <li class="w-1/3">手机：xx（微信同号）</li>
-        <li class="w-1/3">邮箱：xx</li>
+        <li class="w-full md:w-1/3 md:pr-2 break-words box-border">xx（计算机相关专业）</li>
+        <li class="w-full md:w-1/3 md:pr-2 break-words box-border">期望职位：前端开发工程师</li>
+        <li class="w-full md:w-1/3 md:pr-2 break-words box-border">工作年限：3年+</li>
+        <li class="w-full md:w-1/3 md:pr-2 break-words box-border">技术博客：xx</li>
+        <li class="w-full md:w-1/3 md:pr-2 break-words box-border">手机：xx（微信同号）</li>
+        <li class="w-full md:w-1/3 md:pr-2 break-words box-border">邮箱：xx</li>
       </ul>
     </div>
   </section>
