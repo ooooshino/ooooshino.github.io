735d572ce4c82e58ee38c71c8a589bcfcbcd2879
ooooshino <tangyusen9@gmail.com>
2026-03-18 18:14:51 +0800

    chore(简历): 脱敏公开简历敏感信息

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,14 +1,14 @@
 <template>
   <section>
     <div>
-      <h3 class="text-center sm:text-left">唐育森（Kent）</h3>
+      <h3 class="text-center sm:text-left">Kent</h3>
       <ul class="flex flex-wrap">
-        <li class="w-1/3">广东东软学院计算机系</li>
+        <li class="w-1/3">xx（计算机相关专业）</li>
         <li class="w-1/3">期望职位：前端开发工程师</li>
         <li class="w-1/3">工作年限：3年+</li>
-        <li class="w-1/3">技术博客：https://oshino.cc</li>
-        <li class="w-1/3">手机：131-28398-8744（微信同号）</li>
-        <li class="w-1/3">邮箱：tangyusen9@gmail.com</li>
+        <li class="w-1/3">技术博客：xx</li>
+        <li class="w-1/3">手机：xx（微信同号）</li>
+        <li class="w-1/3">邮箱：xx</li>
       </ul>
     </div>
   </section>
